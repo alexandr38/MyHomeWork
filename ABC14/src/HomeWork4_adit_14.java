dce067c154953fb0e2fafec6e865835eac37f064
alexandr38 <aleksandragryzkov@gmail.com>
2016-06-07 10:31:39 +0300

    доделал еще

---
+++ ABC14/src/HomeWork4_adit_14.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Agryzkov on 07.06.2016.
+ */
+
+import java.util.Scanner;
+
+public class HomeWork4_adit_14 {
+
+    /* 14) Определить, является ли шестизначное число "счастливым"
+    (сумма первых трех цифр равна сумме последних трех цифр).
+     */
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); // переменная для работы сканера
+
+        System.out.println(" enter the six-digit number " ); // сообщение пользователю
+        int sixDigNum = sc.nextInt(); // инициализация переменной и ожидание ввода значения
+
+        int sumThreeSenior = sixDigNum / 100000 + ( sixDigNum % 100000 ) / 10000 + ( ( sixDigNum % 100000 ) % 10000) / 1000 ; // сумма трех старших
+        int sumThreeYounger = ( sixDigNum % 1000 ) /100 + ( ( sixDigNum % 1000 ) % 100 ) / 10 + sixDigNum % 10 ;        // сумма трех младших
+        if ( sumThreeSenior == sumThreeYounger )
+            System.out.println( " the number entered is happy " );
+        else
+            System.out.println( " the number entered is not happy " );
+
+
+    }
+}
