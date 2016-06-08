9efda0df8848a2bb4f7d47a15dbc8d8876b3f12b
alexandr38 <aleksandragryzkov@gmail.com>
2016-06-08 16:37:30 +0300

    week2

--- ABC14/src/week2/HomeWork3_4.java
+++ ABC14/src/week2/HomeWork3_4.java
@@ -14,5 +14,19 @@ public class HomeWork3_4 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int [ ] arrayAElem = new int [ 3 ];
+        System.out.println(" enter an array A of three elements " ) ;
+        for ( int i = 0 ; i < arrayAElem.length ; i++ ) {
+            System.out.println(" enter the array A element [" + i + "] ");
+            arrayAElem[i] = sc.nextInt();
+        }
+         int [ ] arrayBElem = new int [ 3 ];
+         System.out.println(" enter an array B of three elements " ) ;
+         for ( int i = 0 ; i < arrayBElem.length ; i++ ) {
+             System.out.println(" enter the array B element [" + i + "] ");
+             arrayBElem[i] = sc.nextInt();
+         }
+
     }
 }
