737b4a6d189083b30f4a58e4d52153e750f701a7
alexandr38 <aleksandragryzkov@gmail.com>
2016-06-10 00:23:02 +0300

    week2

--- ABC14/src/week2/HomeWork5_array.java
+++ ABC14/src/week2/HomeWork5_array.java
@@ -148,7 +148,28 @@ public class HomeWork5_array {
         }
         return group;
     }
-       
+
+    /* Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
+    You may modify and return the given array, or return a new array.
+    shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+    shiftLeft([1, 2]) → [2, 1]
+    shiftLeft([1]) → [1]
+     */
+    public int[] shiftLeft(int[] nums) {
+        switch ( nums.length )  {
+            case 0:
+            case 1:
+                break ;
+            default :
+                int buff = nums[0] ;
+                for ( int i = 0; i < nums.length - 1; i++ ) {
+                    nums[i] = nums[i+1];
+                } ;
+                nums[nums.length-1] = buff;
+                break ;
+        }
+        return nums ;
+    }
 
 
 }
