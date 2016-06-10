144e6ca3ccc51441adb62f03b13b9ef11a6cb0a8
alexandr38 <aleksandragryzkov@gmail.com>
2016-06-10 17:55:52 +0300

    week2

--- ABC14/src/week2/HomeWork6_string_2.java
+++ ABC14/src/week2/HomeWork6_string_2.java
@@ -33,4 +33,30 @@ public class HomeWork6_string_2 {
     countCode("cozexxcope") ? 2
     */
 
+
+    /* Return true if the given string contains a "bob" string,
+    but where the middle 'o' char can be any char.
+    bobThere("abcbob") ? true
+    bobThere("b9b") ? true
+    bobThere("bac") ? false
+    */
+    public boolean bobThere(String str) {
+        boolean bob = false ;
+        switch ( str.length() ) {
+            case 0 :
+            case 1 :
+            case 2 :
+                bob = false ;
+                break ;
+            case 3 :
+                bob = str.charAt(0) == 'b' && str.charAt(2) == 'b';
+                break ;
+            default :
+                for ( int i =1 ; i < str.length() - 2 && !bob ; i++ ) {
+                    bob = str.charAt(i) == 'b' && str.charAt(i + 2) == 'b';
+                }
+                break ;
+        }
+        return bob ;
+    }
 }
