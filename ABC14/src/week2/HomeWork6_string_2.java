1b94ec6fb612a90f9d017a2281415719f95e766a
alexandr38 <aleksandragryzkov@gmail.com>
2016-06-11 11:08:20 +0300

    week2

--- ABC14/src/week2/HomeWork6_string_2.java
+++ ABC14/src/week2/HomeWork6_string_2.java
@@ -59,4 +59,70 @@ public class HomeWork6_string_2 {
         }
         return bob ;
     }
+    /* Given a string, consider the prefix string made of the first N chars of the string.
+    Does that prefix string appear somewhere else in the string? Assume that the string is not empty
+    and that N is in the range 1..str.length().
+    prefixAgain("abXYabc", 1) → true
+    prefixAgain("abXYabc", 2) → true
+    prefixAgain("abXYabc", 3) → false
+     */
+    
+
+    /* Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
+     You may assume that n is between 0 and the length of the string, inclusive.
+    repeatEnd("Hello", 3) → "llollollo"
+    repeatEnd("Hello", 2) → "lolo"
+    repeatEnd("Hello", 1) → "o"
+    */
+    public String repeatEnd(String str, int n) {
+        String result = new String() ;
+        switch (str.length() ) {
+            case 0 :
+                result = "" ;
+                break ;
+            default :
+                if ( n <= str.length() ) {
+                    for ( int i = 0 ; i < n ; i++ ) {
+                        result += str.substring(str.length() - n ) ;
+                    }
+                }
+                else result = "" ;
+        }
+        return result ;
+    }
+
+    /* Returns true if for every '*' (star) in the string, if there are chars both
+    immediately before and after the star, they are the same.
+    sameStarChar("xy*yzz") → true
+    sameStarChar("xy*zzz") → false
+    sameStarChar("*xa*az") → true
+    */
+    public boolean sameStarChar(String str) {
+        boolean result = false ;
+        switch ( str.length() ) {
+            case 0 :
+                result = true ;
+                break;
+            case 1 :
+                result = str.charAt(0) == '*' ;
+                break ;
+            case 2 :
+                result = str.charAt(0) == str.charAt(1) ;
+                break ;
+            default :
+                int posStar = str.indexOf('*') ;
+                if ( posStar == -1 ) result = true ;
+                else if ( posStar == str.length() - 1 ) result = false ;
+                else {
+                    for ( int i = 1 ; i < str.length() -1 ; i++ ) {
+                        if ( str.charAt(i) == '*' )
+                            result = str.charAt(i-1) == str.charAt(i+1) ;
+                    }
+                }
+                break ;
+        }
+        return result ;
+    }
+
+
 }
