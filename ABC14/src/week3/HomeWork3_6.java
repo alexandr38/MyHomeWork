cc7bd564b867d99434d88613aa980aa0e21896cb
alexandr38 <aleksandragryzkov@gmail.com>
2016-06-16 09:21:22 +0300

    MyArrayUtil

--- ABC14/src/week3/HomeWork3_6.java
+++ ABC14/src/week3/HomeWork3_6.java
@@ -13,11 +13,11 @@ public class HomeWork3_6 {
         Scanner sc = new Scanner(System.in);
         System.out.println(" enter the sequence of numbers ");
         String numStr = sc.next();
+        // sequence of '0'
         int[] search0 = MyStringUtil.searchLongestChain( numStr, '0');
-        System.out.println( search0[0] );
-        System.out.println( search0[1] );
+        System.out.println(" ( " + (search0[0]+1) + ", " + (search0[0]+search0[1]) + " ) - chain of 0 ");
+        // sequence of '1'
         int[] search1 = MyStringUtil.searchLongestChain( numStr, '1');
-        System.out.println( search0[0] );
-        System.out.println( search0[1] );
+        System.out.println(" ( " + (search1[0]+1) + ", " + (search1[0]+search1[1]) + " ) - chain of 1 ");
     }
 }
