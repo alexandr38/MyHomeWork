a20abcb9586fc7c29d25075088301f8e823aad64
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-05 10:17:42 +0300

    pravka

---
+++ ACO14/src/week1/Student.java
@@ -0,0 +1,10 @@
+package week1;
+
+/**
+ * Created by Agryzkov on 05.07.2016.
+ */
+public class Student {
+    public String name;
+    public String adress;
+    // Subject
+}
---
+++ ACO14/src/week1/Subject.java
@@ -0,0 +1,10 @@
+package week1;
+
+/**
+ * Created by Agryzkov on 05.07.2016.
+ */
+public class Subject {
+
+    public String name;
+    public int numOfHoursPerSemester;
+}
