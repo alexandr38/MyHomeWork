adb18f4a5e3ef37ca7dd63bd4169c3bdb6f5fe24
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-06 17:41:41 +0300

    oopW1

---
+++ ACO14/src/week1/student/TestSubject.java
@@ -0,0 +1,25 @@
+package week1.student;
+
+/**
+ * Created by Agryzkov on 06.07.2016.
+ */
+public class TestSubject {
+
+    public static void main(String[] args) {
+
+        // create subject
+        Subject subject = new Subject();
+        subject.initSubject("fisica", 20);
+
+        // print info
+        System.out.println(subject.showInfBySubjAsString());
+
+
+        // exam || set
+        subject.setEvaluationBySubject(5);
+        System.out.println(subject.showInfBySubjAsString());
+
+        
+
+    }
+}
