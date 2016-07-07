face63ad27204795c53c28b8c6438c7111149d54
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-07 10:28:47 +0300

    oopW1

--- ACO14/src/week1/contact_list/ContactListMenu.java
+++ ACO14/src/week1/contact_list/ContactListMenu.java
@@ -81,7 +81,8 @@ public class ContactListMenu {
     }
 
     private boolean chekPhone (String phone){
-        boolean chPhone = true;
+
+        boolean chPhone = (phone.length()>=9 && phone.length()<=12);
 
         for (int i = 0; i < phone.length()&& chPhone; i++){
             if ( !(Character.isDigit(phone.charAt(i)))){
