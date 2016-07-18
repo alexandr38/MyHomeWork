14721a11035b4f91a3f60a4afacbc6d5339edf1c
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-18 12:22:21 +0300

    oopW4 codingBut

--- ACO14/src/week4/Recursion_1.java
+++ ACO14/src/week4/Recursion_1.java
@@ -5,17 +5,7 @@ package week4;
  */
 public class Recursion_1 {
 
-    // factorial - recursion
-    public int factorial(int n) {
-
-        int result = 0;
-        if (n == 1){
-            return 1;
-        }
-        result = factorial(n-1)*n;
-        return result;
-    }
-    // ore without variable
+    // factorial without variable
     public int factorial(int n) {
 
         if (n == 1){return 1;}
@@ -36,9 +26,17 @@ public class Recursion_1 {
         else { if (bunnies%2 == 0){
                     return bunnyEars2(bunnies - 1) + 3;
                 } else {return bunnyEars2(bunnies - 1) + 2;}
-             }
+        }
     }
 
+    /*  Given a non-negative int n, return the count of the occurrences of 7 as a digit,
+    * so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+    * while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    *    count7(717) ? 2
+    *   count7(7) ? 1
+    *  count7(123) ? 0
+    */
     
+
 
 }
