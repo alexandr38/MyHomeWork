bae5f5d4d3cd8f78baaca6a360e545c18ac9ec2f
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-18 15:00:51 +0300

    oopW4 codingBut

--- ACO14/src/week4/Recursion_1.java
+++ ACO14/src/week4/Recursion_1.java
@@ -36,7 +36,21 @@ public class Recursion_1 {
     *   count7(7) ? 1
     *  count7(123) ? 0
     */
+
+    public int count7(int n) {
+
+        if (n < 10 && n == 7) {
+            return 1;
+        } else if (n < 10 && !(n == 7)) {
+            return 0;
+        } else if (n % 10 == 7) {
+            return 1 + count7(n / 10);
+        } else return count7(n / 10);
+    }
+
     
+
+
 
 
 }
