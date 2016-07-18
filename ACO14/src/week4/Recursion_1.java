b972290acf2950996b7f6fd1e82b701fcdaeb1ab
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-18 12:19:14 +0300

    oopW4 codingBut

--- ACO14/src/week4/Recursion_1.java
+++ ACO14/src/week4/Recursion_1.java
@@ -30,6 +30,15 @@ public class Recursion_1 {
     *   bunnyEars2(1) ? 2
     *  bunnyEars2(2) ? 5
     */
+    public int bunnyEars2(int bunnies) {
+
+        if (bunnies == 0) {return 0;}
+        else { if (bunnies%2 == 0){
+                    return bunnyEars2(bunnies - 1) + 3;
+                } else {return bunnyEars2(bunnies - 1) + 2;}
+             }
+    }
+
     
 
 }
