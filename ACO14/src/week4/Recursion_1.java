3b13c8e351c402016ab045c7b8bb1e0f479f13dc
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-19 12:01:08 +0300

    oopW4 codingBut

--- ACO14/src/week4/Recursion_1.java
+++ ACO14/src/week4/Recursion_1.java
@@ -263,6 +263,12 @@ public class Recursion_1 {
      *   array220([3], 0) → false
      */
     public boolean array220(int[] nums, int index) {
+
+        if (nums.length == 0 || nums.length - 1 <= index){
+            return false;
+        } else if (index < nums.length) {
+
+        }
         return false;
     }
 
@@ -315,13 +321,72 @@ public class Recursion_1 {
      */
     public boolean strCopies(String str, String sub, int n) {
 
-        return false;
-
+        if (str.length() == 0 || str.length() < sub.length()){
+            return n == 0;
+        } else if (str.indexOf(sub) == -1){
+            return n == 0;
+        } else
+            return strCopies(str.substring(str.indexOf(sub) + 1), sub, n - 1);
     }
 
+    /*    The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition.
+     *  The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the
+     *  sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a
+     *  recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of
+     *  the sequence.
+     *    fibonacci(0) ? 0
+     *    fibonacci(1) ? 1
+     *    fibonacci(2) ? 1
+     */
+    public int fibonacci(int n) {
 
+        if (n == 0){
+            return 0;
+        } else if (n == 1){
+            return 1;
+        } else return n - 1 + fibonacci(n - 1);
+    }
 
+    /*  Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields
+     * the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+     *   sumDigits(126) ? 9
+     *   sumDigits(49) ? 13
+     *   sumDigits(12) ? 3
+     */
+    public int sumDigits(int n) {
 
+        if (n < 10){
+            return n;
+        } else return n % 10 + sumDigits(n / 10);
+    }
 
+    /*  Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power,
+     * so powerN(3, 2) is 9 (3 squared).
+     *    powerN(3, 1) ? 3
+     *    powerN(3, 2) ? 9
+     *    powerN(3, 3) ? 27
+     */
+    public int powerN(int base, int n) {
 
+        if (n == 0){
+            return 1;
+        } else return base *+ powerN(base,n - 1);
+    }
+
+    /*   Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars
+     *  have been changed to 'y' chars.
+     *   changeXY("codex") ? "codey"
+     *   changeXY("xxhixx") ? "yyhiyy"
+     *   changeXY("xhixhix") ? "yhiyhiy"
+     */
+    public String changeXY(String str) {
+
+        if (str.length() == 0){
+            return str;
+        } else if (str.charAt(0) == 'x'){
+            return 'y' + changeXY(str.substring(1));
+        } else return str.charAt(0) + changeXY(str.substring(1));
+    }
+
+    
 }
