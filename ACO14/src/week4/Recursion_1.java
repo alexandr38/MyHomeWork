636d03280c2fed23e5cbce5254100faa0943c569
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-19 16:53:44 +0300

    oopW4 codingBut

--- ACO14/src/week4/Recursion_1.java
+++ ACO14/src/week4/Recursion_1.java
@@ -450,7 +450,7 @@ public class Recursion_1 {
             return str;
         } else if (str.charAt(0) == str.charAt(1)){
             return  stringClean(str.substring(1));
-        }else return String.valueOf(str.charAt(0) + stringClean(str.substring(1)));
+        } else return String.valueOf(str.charAt(0) + stringClean(str.substring(1)));
     }
 
     /*      Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))".
