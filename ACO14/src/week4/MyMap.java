ae2130eecb88bb6fe3f8c81f83b61cdd8bade9ed
alexandr38 <aleksandragryzkov@gmail.com>
2016-07-20 14:37:10 +0300

    oopW4 codingBut_Map

--- ACO14/src/week4/MyMap.java
+++ ACO14/src/week4/MyMap.java
@@ -277,8 +277,45 @@ public class MyMap {
         Map<String, Integer> map = new HashMap<String, Integer>();
         String result = "";
 
-        
+        for (String key : strings){
+            if (map.containsKey(key)){
+                result += key;
+                map.remove(key);
+            } else
+                map.put(key, 1);
+        }
         return result;
     }
 
+    /*      We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and
+     * then return the given array of non-empty strings as follows: if a string matches an earlier string in the array,
+     * swap the 2 strings in the array. A particular first char can only cause 1 swap, so once a char has caused a swap,
+     * its later swaps are disabled. Using a map, this can be solved making just one pass over the array. More difficult
+     * than it looks.
+     *   firstSwap(["ab", "ac"]) → ["ac", "ab"]
+     *   firstSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "aaa", "azz"]
+     *   firstSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "ai", "aj", "bx", "by"]
+     */
+    public String[] firstSwap(String[] strings) {
+
+        if (strings.length > 0){
+
+            Map<String, Integer> map = new HashMap<String, Integer>();
+
+            for (int i = 0; i < strings.length; i++){
+                if (!strings[i].isEmpty()){
+                    if (map.containsKey(String.valueOf(strings[i].charAt(0)))){
+                        if (map.get(String.valueOf(strings[i].charAt(0))) != -1){
+                            String buff = strings[map.get(String.valueOf(strings[i].charAt(0)))];
+                            strings[map.get(String.valueOf(strings[i].charAt(0)))] = strings[i];
+                            strings[i] = buff;
+                            map.put(String.valueOf(strings[i].charAt(0)), -1);
+                        }
+                    } else
+                        map.put(String.valueOf(strings[i].charAt(0)), i);
+                }
+            }
+        }
+        return strings;
+    }
 }
