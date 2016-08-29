2fd83d93a22d5c27a8c9440117e7d034b3fbf339
alexandr38 <aleksandragryzkov@gmail.com>
2016-08-29 17:55:28 +0300

    list

--- ACO14/src/week5/main/list/MyArrayList.java
+++ ACO14/src/week5/main/list/MyArrayList.java
@@ -11,17 +11,17 @@ public class MyArrayList<E> implements IMyList<E> {
 
     private E[] elementData;
     private final int DEFAULT_CAPACITY = 5;
-    private int size, capacity;
+    private int size, index;
 
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
@@ -36,17 +36,41 @@ public class MyArrayList<E> implements IMyList<E> {
 
     @Override
     public boolean add(E element) {
-        return false;
+        if (size + 1 > elementData.length){
+            resizeArray();
+        }
+        elementData[size++] = element;
+        return true;
     }
 
     @Override
     public void add(int index, E element) {
+        if (size + 1 > elementData.length){
+            resizeArray();
+        }
 
+        E[] temp = (E[]) new Object[elementData.length];
+
+        if (index == 0){
+            temp[0] = element;
+            System.arraycopy(elementData, 0, temp, 1, size);
+            } else {
+                if (index == size) {
+                    temp[size] = element;
+                    System.arraycopy(elementData, 0, temp, 0, size);
+                } else {
+                    System.arraycopy(elementData, 0, temp, 0, index - 1);
+                    temp[index - 1] = element;
+                    System.arraycopy(elementData, index - 1, temp, index, size - index + 1);
+                }
+        }
+        elementData = temp;
+        size++;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return elementData[index - 1];
     }
 
     @Override
@@ -152,11 +176,17 @@ public class MyArrayList<E> implements IMyList<E> {
 
     public MyArrayList() {
         elementData = (E[]) new Object[DEFAULT_CAPACITY];
-        size = 0;
+        this.index = this.size = 0;
     }
 
     public MyArrayList(int capacity) {
         elementData = (E[]) new Object[capacity];
-        size = 0;
+        this.index = this.size = 0;
+    }
+
+    private void resizeArray() {
+        E[] temp = (E[]) new Object[elementData.length * 2 / 3 - 1];
+        System.arraycopy(elementData, 0, temp, 0, elementData.length);
+        elementData = temp;
     }
 }
